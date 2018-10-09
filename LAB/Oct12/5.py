edd2d5411f8a08a9038e728084325c76c0835712
thanaphon <thanaphon.tang@gmail.com>
2018-10-09 12:41:15 +0700

    Create 5.py

---
+++ LAB/Oct12/5.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  9 12:12:15 2018
+
+MORE on test cases
+
+@author: computer
+"""
+def comp(x):
+    if x<3:
+        print('A')
+    elif x > 10 :
+        print('B')
+    else:
+        print('C')
+        
+# What is the max number of print  a function comp(x) can printout  for an input x? 
+     
+
+
+        
+# Give the smallest set (possible) of test cases that exploreall the possible outcome of comp
+
+        
+
+
+# Explain the specification of comp in word 
+
+         
+        
+        
+def comp2(x,y):
+    if x == y:
+        print('A', end =' ')
+    elif x < 5 and y > 2:
+        print('B', end =' ')
+    if x > 2 or y > 4:
+        print('C', end= ' ')
+    
+    
+# What is the maximum number of print that a function comp2(x,y) can call for an input (x,y)? 
+        
+        
+        
+      
+# How many are the possible printing outputs this code can produce  for an input (x,y)? 
+
+
+        
+        
+# Give the smallest set (possible) of test cases that explore all the possible outcome of comp2
+#also explain your test cases. 
+  
+        
+    
+
