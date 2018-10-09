75ab477d771e605eaa4e2633c0ec91266c2a5b41
thanaphon <thanaphon.tang@gmail.com>
2018-10-09 11:48:19 +0700

    Create 4.py

---
+++ LAB/Oct12/4.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Goal: TEST CASES 
+    Black box testing 
+    2.1.1 given a function specification
+    come up with hypothesis followed by test cases that might break the code 
+    
+    fill out the following table
+    
+    
+    
+    
+    Glass box testing
+    2.1 Identify the test cases that give path-complete for the given functions
+    2.2 Understand the basic syntax for try and catch  
+    
+"""
+
+
+
